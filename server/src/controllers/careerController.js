b36d1043748de74af33c2aca0144b32787439452
ayush31082005 <ayushchaubey19768@gmail.com>
2026-02-08 20:47:22 +0530

    Update careerController.js

--- server/src/controllers/careerController.js
+++ server/src/controllers/careerController.js
@@ -1,8 +1,29 @@
-// Generic controller placeholder
-exports.getAll = (req, res) => {
-    res.json({ message: 'Not implemented yet' });
+const Career = require("../models/Career");
+
+/* SUBMIT APPLICATION */
+exports.submitCareer = async (req, res) => {
+    try {
+        const career = await Career.create({
+            ...req.body,
+            resume: req.file?.filename
+        });
+
+        res.status(201).json({
+            success: true,
+            message: "Application submitted",
+            career
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
-exports.create = (req, res) => {
-    res.json({ message: 'Not implemented yet' });
+/* GET ALL APPLICATIONS */
+exports.getCareers = async (req, res) => {
+    try {
+        const data = await Career.find().sort({ createdAt: -1 });
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
